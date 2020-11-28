d86ea95b23add83e46f3a1968d510b25c299b6e1
Vichhika <vichhikapann@gmail.com>
2020-11-28 11:33:07 +0700

    fix

--- Homework 07/Recommendation_Sys/src/App.java
+++ Homework 07/Recommendation_Sys/src/App.java
@@ -7,6 +7,7 @@ public class App {
     private static Connection conn;
     private static PreparedStatement preStatement;
     private static ResultSet resultStatement;
+    private static Statement statement;
     private static Scanner input = new Scanner(System.in);
     private static String username;
     private static String password;
@@ -31,15 +32,35 @@ public class App {
             System.out.println("____Login____");
             System.out.print("Username:");
             username = input.nextLine();
-            System.out.print("Username:");
+            System.out.print("Password:");
             password = input.nextLine();
             if(user.verify(username, password, conn.createStatement(), resultStatement)){
-                
+                System.out.println("_____Movie_Menu_____");
+                /**
+                 * Automatically listing movies following by requirement of menu 1-5
+                 */
+                System.out.println("1. Most popular movies");
+                System.out.println("2. Most popular movies in your country");
+                System.out.println("3. Movies by author");
+                System.out.println("4. Hot movies");
+                System.out.println("5. Movies that most users like");
+                System.out.println("6. Rating movies"); //List all movies to provide user to like or dislike
+                System.out.println("0. Logout");
             }
         }
     }
 
     public static void main(String[] args) throws Exception {
         menu();
+
+        /**
+         * testing statement
+         */
+        // statement = conn.createStatement();
+        // resultStatement = statement.executeQuery("Statement");
+        // while(resultStatement.next()){
+        //     String result = resultStatement.getString("parameter");
+        //     System.out.println(result);
+        //}
     }
 }
