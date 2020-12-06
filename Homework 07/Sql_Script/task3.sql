3cd2b06a9bf527e9607d55a8e124560e6d268d10
Sambath <dilytithsambath@gmail.com>
2020-12-06 17:53:11 +0700

    Recommit task 3 and task 5

--- Homework 07/Sql_Script/task3.sql
+++ Homework 07/Sql_Script/task3.sql
@@ -1,14 +1,20 @@
 
 SELECT Movie.Title
-FROM ​ Movie INNER JOIN (Author INNER JOIN ​MovieDetail ON Author.AuthorID = MovieDetail.AuthorID) ON Movie.MovieID = ​MovieDetail.MovieID
+FROM ​Movie INNER JOIN (
+    Author INNER JOIN ​MovieDetail ON Author.AuthorID = MovieDetail.AuthorID
+    ) ON Movie.MovieID = ​MovieDetail.MovieID
+
 WHERE Author.AuthorID IN (
 SELECT Author.AuthorID
-FROM ​ [User] INNER JOIN([Like] INNER JOIN (Movie INNER JOIN (Author INNER JOIN ​MovieDetail ON Author.AuthorID = MovieDetail.AuthorID) ON Movie.MovieID = ​MovieDetail.MovieID) ON [Like].MovieID = Movie.MovieID) ON [User].UserID = [Like].UserID
+FROM ​ [User] INNER JOIN([Like] INNER JOIN (Movie INNER JOIN (Author INNER JOIN ​MovieDetail ON Author.AuthorID = MovieDetail.AuthorID) 
+ON Movie.MovieID = ​MovieDetail.MovieID) ON [Like].MovieID = Movie.MovieID) ON [User].UserID = [Like].UserID
 WHERE [User].UserID = 5 AND LikeOrDislike = 'yes' 
 GROUP BY Author.AuthorID
-) AND Movie.MovieID NOT IN ( SELECT Movie.MovieID
-FROM [User] INNER JOIN (Movie INNER JOIN [Like] ON Movie.MovieID = [Like].MovieID) ON [User].UserID = [Like].UserID
-WHERE [User].UserID = 5 AND LikeOrDislike = 'yes')
+) AND Movie.MovieID NOT IN ( 
+SELECT [Like].MovieID
+FROM [User] INNER JOIN [Like] ON [User].UserID = [Like].UserID
+WHERE [User].UserID = 5 AND LikeOrDislike = 'yes'
+)
 
 
 
--- Homework 07/Sql_Script/task5.sql
+++ Homework 07/Sql_Script/task5.sql
@@ -1,6 +1,5 @@
 /*In case want to get userid and MovieID we can select it but need to remove group by*/
 /*The code below only out put the movies that need to recommend to current user only*/
-/*But this query below is not eliminate the movie that have less watch out yet*/
 SELECT [Movie].Title
 FROM [Like] INNER JOIN [Movie] ON [Like].MovieID = [Movie].MovieID
 WHERE [Like].UserID IN (
@@ -9,6 +8,10 @@ WHERE [Like].UserID IN (
                         WHERE [Like].MovieID = 1 AND LikeOrDislike = 'yes'
                     ) 
                     AND [Like].MovieID != 1 
-                    AND LikeOrDislike = 'yes' 
-                    AND [Like].UserID != 1
+                    AND LikeOrDislike = 'yes'
+                    AND [Like].MovieID NOT IN (
+                        SELECT [Like].[MovieID]
+                        FROM [Like]
+                        WHERE [Like].[UserID] = 1
+                    ) 
 GROUP BY Movie.Title
